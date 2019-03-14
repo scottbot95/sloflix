01b164564f7da56fb7efe4d1ce3cbc404b39e9bc
Scott Techau <scotttechau@gmail.com>
2019-03-14 14:54:21 -0700

    feat: Create UserService to handle loggin in and out

--- ClientApp/src/app/shared/services/base.service.ts
+++ ClientApp/src/app/shared/services/base.service.ts
@@ -1,5 +1,26 @@
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs';
 
 export abstract class BaseService {
   constructor() {}
+
+  protected handleError(error: any) {
+    let applicationError = error.headers.get('Application-Error');
+
+    if (applicationError) {
+      return Observable.throw(applicationError);
+    }
+
+    let modelStateErrors: string = '';
+    let serverError = error.json();
+
+    if (!serverError.type) {
+      for (let key in serverError) {
+        if (serverError.hasOwnProperty(key))
+          modelStateErrors += serverError[key] + '\n';
+      }
+    }
+
+    modelStateErrors = modelStateErrors === '' ? null : modelStateErrors;
+    return Observable.throw(modelStateErrors || 'Server error');
+  }
 }
--- ClientApp/src/app/shared/services/user.service.ts
+++ ClientApp/src/app/shared/services/user.service.ts
@@ -1,9 +1,65 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable, BehaviorSubject } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class UserService {
+import { ConfigService } from './config.service';
+import { BaseService } from './base.service';
+import { UserRegistration } from '../models/user.registration.interface';
+import '../../rxjs-operators';
 
-  constructor() { }
+@Injectable()
+export class UserService extends BaseService {
+  baseUrl: string = '';
+
+  // Observable navItem source
+  private _authNavStatusSource = new BehaviorSubject<boolean>(false);
+  // Observable navItem stream
+  authNavStatus$ = this._authNavStatusSource.asObservable();
+
+  private loggedIn = false;
+
+  constructor(private http: Http, private configService: ConfigService) {
+    super();
+    this.loggedIn = !!localStorage.getItem('auth_token');
+    this._authNavStatusSource.next(this.loggedIn);
+    this.baseUrl = configService.getApiURI();
+  }
+
+  register(email: string, password: string): Observable<UserRegistration> {
+    let body = JSON.stringify({ email, password });
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers });
+
+    return this.http
+      .post(this.baseUrl + '/auth/accounts', body, options)
+      .map(res => true)
+      .catch(this.handleError);
+  }
+
+  login(email: string, password: string) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    const body = JSON.stringify({ email, password });
+    return this.http
+      .post(this.baseUrl + '/auth/accounts/login', body)
+      .map(res => res.json())
+      .map(res => {
+        localStorage.setItem('auth_token', res.auth_token);
+        this.loggedIn = true;
+        this._authNavStatusSource.next(true);
+        return true;
+      })
+      .catch(this.handleError);
+  }
+
+  logout() {
+    localStorage.removeItem('auth_token');
+    this.loggedIn = false;
+    this._authNavStatusSource.next(false);
+  }
+
+  isLoggedIn() {
+    return this.loggedIn;
+  }
 }
