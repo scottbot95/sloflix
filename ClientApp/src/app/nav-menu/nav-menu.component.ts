aaa807b47d134f9df590078e0fd2d8a4c2f7e570
Scott Techau <scotttechau@gmail.com>
2019-03-15 14:25:43 -0700

    feat: Change navlinks based on auth status

--- ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from '../shared/services/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent {
-  isExpanded = false;
+export class NavMenuComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+
+  isExpanded: boolean = false;
+  isLoggedIn: boolean = false;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.subscription = this.userService.authNavStatus$.subscribe(loggedIn => {
+      this.isLoggedIn = loggedIn;
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
   collapse() {
     this.isExpanded = false;
