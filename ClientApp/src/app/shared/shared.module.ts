f6e7a106ccf11fef9f41e283dfe0d1a980a9d977
Scott Techau <scotttechau@gmail.com>
2019-03-18 11:56:58 -0700

    refactor: Move directive to shared module

--- ClientApp/src/app/shared/shared.module.ts
+++ ClientApp/src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { UserService } from './services/user.service';
+import { EqualValidator } from './directives/equal-validator.directive';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [SpinnerComponent],
-  exports: [SpinnerComponent],
+  declarations: [SpinnerComponent, EqualValidator],
+  exports: [SpinnerComponent, EqualValidator],
   providers: [UserService]
 })
 export class SharedModule {}
