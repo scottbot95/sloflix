4296243f432a1bad8c80956389c0ebc3ca03917d
Scott Techau <scottbot95@gmail.com>
2019-03-21 13:56:38 -0700

    docs: Move docs to interface file

--- Services/IMovieService.cs
+++ Services/IMovieService.cs
@@ -10,8 +10,16 @@ namespace sloflix.Services
   {
     IQueryable<Movie> GetAllMovies();
     IQueryable<Movie> SearchMovies(string title);
-    Task RateMovieAsync(Claim userId, int movieId, int rating);
     Task DeleteAsync(Claim userId, int movieId);
     Task<Movie> CreateMovieAsync(MovieDto data);
+
+    /// <summary>
+    /// Set the rating on a movie based off the claim provided
+    /// </summary>
+    /// <param name="userId">Claim of user requesting the rating change</param>
+    /// <param name="movieId">Id of movie to rate</param>
+    /// <param name="rating">The rating to set for the movie (0-5) (0 means remove rating)</param>
+    /// <returns></returns>
+    Task RateMovieAsync(Claim userId, int movieId, int rating);
   }
 }
--- Services/MovieService.cs
+++ Services/MovieService.cs
@@ -48,13 +48,6 @@ namespace sloflix.Services
       return _dataContext.Movies;
     }
 
-    /// <summary>
-    /// Set the rating on a movie based off the claim provided
-    /// </summary>
-    /// <param name="userId">Claim of user requesting the rating change</param>
-    /// <param name="movieId">Id of movie to rate</param>
-    /// <param name="rating">The rating to set for the movie (0-5) (0 means remove rating)</param>
-    /// <returns></returns>
     public async Task RateMovieAsync(Claim userId, int movieId, int rating)
     {
       var watcher = await _dataContext.GetWatcherFromClaim(userId);
