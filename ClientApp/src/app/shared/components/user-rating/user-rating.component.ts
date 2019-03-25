2cddf13867284b51262506297340f65edffa5ac5
Scott Techau <scottbot95@gmail.com>
2019-03-25 11:31:00 -0700

    feat: Allow clicking on rating to set rating

--- ClientApp/src/app/shared/components/user-rating/user-rating.component.ts
+++ ClientApp/src/app/shared/components/user-rating/user-rating.component.ts
@@ -1,14 +1,54 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, OnDestroy } from '@angular/core';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-rating',
   templateUrl: './user-rating.component.html',
   styleUrls: ['./user-rating.component.css']
 })
-export class UserRatingComponent implements OnInit {
+export class UserRatingComponent implements OnChanges, OnInit, OnDestroy {
   @Input() rating: number = 0;
+  @Input() setRating: (number) => void;
+
+  private displayRating: number = 0;
+  private ratingHover = new BehaviorSubject<number>(0);
+  private subscription: Subscription;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnChanges() {
+    this.displayRating = this.rating;
+  }
+
+  ngOnInit() {
+    this.subscription = this.ratingHover
+      .pipe(
+        debounceTime(50),
+        distinctUntilChanged()
+      )
+      .subscribe(r => this.setDisplayRating(r));
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  handleHover(rating) {
+    this.ratingHover.next(rating);
+  }
+
+  handleClick(rating) {
+    if (this.rating === rating) {
+      rating = 0;
+    }
+    this.rating = rating;
+    if (typeof this.setRating === 'function') {
+      this.setRating(rating);
+    }
+  }
+
+  setDisplayRating(rating) {
+    this.displayRating = rating;
+  }
 }
